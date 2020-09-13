716b89350a5863a94f4e4ffaa625068aacb0f86b
NimalNallasamy <nimal.nallasamy@gmail.com>
2020-09-14 00:37:23 +0530

    Source Code pushing.

---
+++ src/main/java/com/ekart/thunderbolt/ThunderboltApplication.java
@@ -0,0 +1,13 @@
+package com.ekart.thunderbolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderboltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderboltApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekart/thunderbolt/ThunderboltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekart.thunderbolt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderboltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
