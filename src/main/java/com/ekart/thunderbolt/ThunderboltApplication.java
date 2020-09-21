7308ca4fb22b9d321b624c6c82ce67440f0de847
NimalNallasamy <nimal.nallasamy@gmail.com>
2020-09-22 01:05:36 +0530

    Swagger Configuration Changes

--- src/main/java/com/ekart/thunderbolt/ThunderboltApplication.java
+++ src/main/java/com/ekart/thunderbolt/ThunderboltApplication.java
@@ -1,9 +1,14 @@
 package com.ekart.thunderbolt;
 
+import com.ekart.thunderbolt.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+@Import(SwaggerConfiguration.class)
 public class ThunderboltApplication {
 
 	public static void main(String[] args) {
