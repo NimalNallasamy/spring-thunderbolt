baa1f377f856718460fc301d98970921e699e31d
NimalNallasamy <nimal.nallasamy@gmail.com>
2020-09-27 14:09:23 +0530

    Updated the configuration to Open API v3. And removed unnecessary changes.

--- src/main/java/com/ekart/thunderbolt/ThunderboltApplication.java
+++ src/main/java/com/ekart/thunderbolt/ThunderboltApplication.java
@@ -1,6 +1,5 @@
 package com.ekart.thunderbolt;
 
-import com.ekart.thunderbolt.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -8,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@Import(SwaggerConfiguration.class)
+//@Import(SwaggerConfiguration.class)
 public class ThunderboltApplication {
 
 	public static void main(String[] args) {
