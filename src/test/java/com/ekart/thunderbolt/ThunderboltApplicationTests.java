5f46514e7fe529e57b7119274d5e7b0c987663ab
NimalNallasamy <nimal.nallasamy@gmail.com>
2020-12-10 00:28:32 +0530

    Added Mock Environment to fix building issues

--- src/test/java/com/ekart/thunderbolt/ThunderboltApplicationTests.java
+++ src/test/java/com/ekart/thunderbolt/ThunderboltApplicationTests.java
@@ -1,9 +1,18 @@
 package com.ekart.thunderbolt;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = ThunderboltApplication.class
+)
+@AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application-integrationtesting.yaml"
+)
 class ThunderboltApplicationTests {
 
 	@Test
